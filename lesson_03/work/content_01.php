c74e95dcefde63414ab5ca7a8c688ca60bce773a
Jesse <jesse@opencart.com>
2017-05-23 15:10:14 +0800

    Testing

---
+++ lesson_03/work/content_01.php
@@ -0,0 +1,17 @@
+  <?php include 'header.php'; ?>
+
+  <div>
+
+  <h1>Content 1</h1>
+   content here
+
+  </div>
+
+  <ul class="list-example">
+    <li>Hello!</li>
+    <li>Ohayou!</li>
+    <li>DIU LI</li>
+    <li>DAMN!!!</li>
+  </ul>
+
+  <?php include 'footer.php'; ?>
